ec416f997c18bd18424c1874515ba98b413febf9
asaklil <106289658+asaklil@users.noreply.github.com>
2023-02-06 20:04:23 -0500

    Update d4q4.py

--- d4q4.py
+++ d4q4.py
@@ -1,30 +1,8 @@
-﻿#Nom: Mohamed Ismail Asaklil
-#Numero d'etudiant: 0300243534
 
-#QUESTION 4
-
-
-
-###################################################################################################################################
-
-
-
-
-
-# Jeu de cartes.
-
-# L'ordinateur est le donneur des cartes.
-
-# Une carte est une chaine de 2 caractères. 
-# Le premier caractère représente une valeur et le deuxième une couleur.
-# Les valeurs sont des caractères comme '2','3','4','5','6','7','8','9','10','J','Q','K', et 'A'.
-# Les couleurs sont des caractères comme : ♠, ♡, ♣, et ♢.
-# On utilise 4 symboles Unicode pour représenter les 4 couleurs: pique, coeur, trèfle et carreau.
-# Pour les cartes de 10 on utilise 3 caractères, parce que la valeur '10' utilise deux caractères.
 
 import random
 
-#ces premieres fonctions sont deja completees pour vous, ne les changez pas
+
 
 def attend_le_joueur():
     '''()->None
@@ -56,32 +34,22 @@ def melange_paquet(p):
     '''
     random.shuffle(p)
     
-#######################################################################################
-#Les fonctions precedentes sont deja completees
-#Completez les fonctions apres ce commentaire
-#######################################################################################
+
     
 def donne_cartes(p):
-     '''(list of str)-> tuple of (list of str,list of str)
-
-     Retournes deux listes qui représentent les deux mains des cartes.  
-     Le donneur donne une carte à l'autre joueur, une à lui-même,
-     et ça continue jusqu'à la fin du paquet p.
-     '''
+     
      
      donneur=[]
      autre=[]
 
 
-     # COMPLETEZ CETTE FONCTION EN CONFORMITE AVEC LA DESCRIPTION CI-DESSUS
-     # AJOUTEZ VOTRE CODE ICI
+     
      
      for i in range(0,len(p)):
-         if i%2==0: #VU QUE C EST LE ROBOT QUI DISTRIBUE DONC IL DONNE LA PREMIER CARTE A HUMAIN ET INDEX DE LA PREMIERE CARTE EST 0 DONC PAIRE
-#TANDIS QUE CELLE QUI VA DONNER A LUI MEME SON INDEX EST 1 DONC IMPAIRE
+         if i%2==0: 
              autre.append(p[i])
          else:
-             donneur.append(p[i]) #faire integrer des elements a la liste de cartes       
+             donneur.append(p[i])        
      return (donneur, autre)
 
 
@@ -104,17 +72,13 @@ def elimine_paires(l):
     resultat=[]
     l.sort()
 
-    # COMPLETEZ CETTE FONCTION EN CONFORMITE AVEC LA DESCRIPTION CI-DESSUS
-    # AJOUTEZ VOTRE CODE ICI
+   
     resultat = l 
-    compteur= 0 #il faut faire un compteur car si on elimine un doublon et qu il y a un autre doublon qui le suit
-    #il faut que l'index s initialise cad vu que les premiers doublons d index 0 et 1 sont supp il faut que ceux qui les suivent
-    #aient le meme index cad PRENDRE LEURS POSITIONS
+    compteur= 0 
     for i in range(len(resultat) + compteur):
         if i < (len(resultat)+ compteur - 1 ):
-            if resultat[i - compteur][0] == resultat[i+1 - compteur][0]: #J AI AJOUTE LE COMPTEUR CAR SI PAR EXEMPLE DEUX ELEMENTS S'ENLEVENT DONC L'INDEX DE CEUX QUI LES SUIVENT VA PRENDRE LA POSITION DE CEUX SUPPRIMES
-                resultat = resultat[0:i - compteur] + resultat[i+2 - compteur:]#pour enregistrer seulement des le debut de la liste vers le premier doublon(exclus) puis du deuxieme doublon (exclus) jusqu'a len()-1
-                compteur += 1 #INCREMENTER LE COMPTEUR A L INTR DE IF
+            if resultat[i - compteur][0] == resultat[i+1 - compteur][0]: 
+                resultat = resultat[0:i - compteur] + resultat[i+2 - compteur:]
     random.shuffle(resultat)
     return resultat
 
@@ -126,13 +90,11 @@ def affiche_cartes(p):
     '''
 
 
-    # COMPLETEZ CETTE FONCTION EN CONFORMITE AVEC LA DESCRIPTION CI-DESSUS
-    # AJOUTEZ VOTRE CODE ICI
-    resultat="" #str vide
+    resultat="" 
     for i in range(0,len(p)):
-        resultat=resultat+" "+p[i] #des str separes par espaces
+        resultat=resultat+" "+p[i] 
         if i==len(p)-1:
-            print(resultat)#afficher le resultat
+            print(resultat)
         
     
 
@@ -148,14 +110,13 @@ def entrez_position_valide(n):
      '''
     
 
-     # COMPLETEZ CETTE FONCTION EN CONFORMITE AVEC LA DESCRIPTION CI-DESSUS
-     # AJOUTEZ VOTRE CODE ICI
+    
      n>=1
      x=int(input("SVP entrer un entier de "+str(1)+" à "+str(n)+" : "))
-     if x>=1 and x<=n: #le if avant la boucle pour valider le cas ou le joueur entre un nb entre 1 et n des la premiere tentaive
+     if x>=1 and x<=n:
          print("Vous avez demande ma "+str(x)+"ème carte.") 
      else:
-         while x<1 or x>n: #le while a la place du for car on ne sait pas le nb de boucle
+         while x<1 or x>n:
              x=int(input("SVP entrer encore une fois un entier de "+str(1)+" à "+str(n)+" : "))
              if x>=1 and x<=n:
                  print("Vous avez demande ma "+str(x)+"ème carte.")
@@ -186,43 +147,36 @@ def joue():
      donneur=elimine_paires(donneur)
      humain=elimine_paires(humain)
 
-     # COMPLETEZ CETTE FONCTION EN CONFORMITE AVEC LA DESCRIPTION CI-DESSUS
-     # AJOUTEZ VOTRE CODE ICI
+     
      i=0
-     while i>=0: #on utilise le while car on ne sait pas en cm de tour la partie de carte va se finir
-    #le nb de boucle indefini ==> l'utilisation de WHILE
-         if i%2==0: #on constate que c est tjrs l'Humain qui commence a jouer car c est ROBOT qui a distribué les cartes
+     while i>=0: 
+         if i%2==0: 
              print("Votre tour")
              print("Votre main est:")
              affiche_cartes(humain)
              print("J'ai "+str(len(donneur))+" cartes. Si 1 est la position de ma première carte et "+str(len(donneur))+" est la position de ma dernière carte, laquelle de mes cartes vous voulez?") 
              entrez_position_valide(len(donneur))
-             x=donneur[random.randint(1,len(donneur)-1)]#j'utilise la fct random pour l'index pour que ca me donne une carte au hasard de Robot.
-             donneur.remove(x) #vu que l Humain va prendre la carte donc elle n'appartiendra plus a la liste de Robot
-             #il faut la supprimer par consequent
+             x=donneur[random.randint(1,len(donneur)-1)]
+             donneur.remove(x) 
              print("La voilà. C'est un ",x)
              print("Avec "+str(x)+" ajouté, votre main est:")
-             humain.append(x) #pour ajouter la carte prise de chez Robot dans la liste des cartes de Humain
+             humain.append(x) 
              affiche_cartes(humain) 
-             #DEFAUSSER LES PAIRES ET MELANGER LES CARTES DE CETTE LISTE
              print("Après défaussé toutes les paires et mélanger les cartes, votre main est:")
              humain=elimine_paires(humain)
              affiche_cartes(humain)
              attend_le_joueur()
-             i+=1 ####### Il ne faut pas oublier d'incrementer le compteur pour que le tour soit au prochain
-######NE PAS L INCREMENTER A L EXT DU IF STATEMENT CAR SINN ON NE POURRA PAS AVOIR UNE CONTRE CONDITION POUR STOPPER LA BOUCLE
+             i+=1 
              
          else:
              print("Mon tour.")
-             Y=random.randint(1,len(humain)-1)#je note la variable Y qui est un random de la derniere liste a la fin du 1er tour
-             print("J'ai pris votre "+str(Y)+"ème carte.") #on a utilise le random pour choisir au hasard
-             #IL NE FAUT PAS OUBLIER QUE CA DOIT CHOISIR UN NB AU HASARD PAR RAPPORT A LEN() DE LA LISTE REDUITE CAD APRES AVOIR SEPARER LES PAIRES DEUX FOIS
-             donneur.append(humain[Y-1])#car le Robot a pris une de mes cartes
-             humain.remove(humain[Y-1]) #vu que le Robot va prendre ma carte du coup je dois la supp de mes cartes
+             Y=random.randint(1,len(humain)-1)
+             print("J'ai pris votre "+str(Y)+"ème carte.") 
+             donneur.append(humain[Y-1])
+             humain.remove(humain[Y-1]) 
              donneur=elimine_paires(donneur) 
              attend_le_joueur()
-             i+=1 ####### Il ne faut pas oublier d'incrementer le compteur pour que le tour soit au prochain
-######NE PAS L INCREMENTER A L EXT DU IF STATEMENT CAR SINN ON NE POURRA PAS AVOIR UNE CONTRE CONDITION POUR STOPPER LA BOUCLE            
+             i+=1            
 
          if len(donneur)==1 and len(humain)!=1: #DANS LE CAS OU LA LISTE DE CARTE DE ROBOT CONTIENT 1 CARTE DONC C EST ROBOT QUI A GAGNE
              print("J'ai terminé toutes les cartes.")
